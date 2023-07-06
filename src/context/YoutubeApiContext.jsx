c4bcb2762d59b4bcfcb2e7bce26a9ee6ca97ef71
heroyooi <heroyooi1018@gmail.com>
2023-07-06 16:51:33 +0900

    유튜브 리팩토링

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,11 +1,13 @@
 import { createContext, useContext } from "react";
-import FakeYoutube from "../api/fakeYoutube";
 import Youtube from "../api/youtube";
+import FakeYoutubeClient from "../api/fakeYoutubeClient";
+// import YoutubeClient from "../api/youtubeClient";
 
 export const YoutubeApiContext = createContext();
 
-// const youtube = new FakeYoutube();
-const youtube = new Youtube();
+const client = new FakeYoutubeClient();
+// const client = new YoutubeClient();
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
   return (
